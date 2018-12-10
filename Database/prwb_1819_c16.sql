84f8339de6a7918f1640dbbd89c20502b6219af4
jodamn <j.knuts@icloud.com>
2018-12-10 16:12:54 +0100

    Add utilisateurs dans bdo

--- Database/prwb_1819_c16.sql
+++ Database/prwb_1819_c16.sql
@@ -43,6 +43,14 @@ CREATE TABLE `user` (
 --
 -- Index pour les tables déchargées
 --
+-- Contenu de la table
+
+INSERT INTO `user` (`id`, `username`, `password`, `fullname`, `email`, `birthdate`, `role`) VALUES
+(1, 'Admin', 'd5556e1823de73914436569b68d746d7a405d782', 'Administrateur', 'admin@site.be', '1988-07-23', 'admin'),
+(2, 'Manager', '179c29e9a6292d1f79d2fc8a12a53971330e8375', 'Manager', 'manager@site.be', '1988-07-16', 'manager'),
+(3, 'User', 'a3b9c9592260102cf284b18949a2da4cd865b6f1', 'User', 'user@site.be', '1988-01-13', 'member');
+
+--
 
 --
 -- Index pour la table `user`
@@ -62,6 +70,8 @@ ALTER TABLE `user`
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
